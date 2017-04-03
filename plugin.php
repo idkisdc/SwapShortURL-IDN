0fa7497236294217aba2f60d9671696768ffa502
Cc Day <c@8.vu>
2017-04-03 11:12:12 -0400

    Add files via upload
    First update, all variables updated (uniquified) and code trimmed to wat add-on mods.

--- plugin.php
+++ plugin.php
@@ -1,60 +1,44 @@
 <?php
 /*
-Plugin Name: YOURLS IDN
-Plugin URI: http://yourls.org/
-Description: Compatibility with Internationalized Domain Names (IDN). Please read the plugin's readme.txt for configuration information
-Version: 1.0
-Author: Ozh
-Author URI: http://ozh.org/
+Plugin Name: Swap Short URL IDN
+Plugin URI: https://github.com/idkisdc/SwapShortURL-IDN
+Description: Provides Swap Short URL plugin Compatibility with Internationalized Domain Names (IDN)
+Version:     1.00 a
+Author: idkisdc
+Author URI: http://0by.Cc/
 
 Thanks to contributors of the Issue #331 and to Bruno Kerouanton for their input and help
 See http://code.google.com/p/yourls/issues/detail?id=331
+
+This is a clone os Ozh's plugin to provide IDN support to YOURLS_SHORT_URL instead of YOURLS_SITE for the Swap Short Yourls Plugin
 */
 
 // Load & instantiate the IDN library
-yourls_add_action( 'plugins_loaded', 'ozh_yidn_load' );
-function ozh_yidn_load() {
-	require_once ( dirname( __FILE__ ).'/IDNA2.php' );
-	$idn = Net_IDNA2::getInstance();
+yourls_add_action( 'plugins_loaded', 'zcc_ssuidn_load' );
+function zcc_ssuidn_load() {
+	require_once ( dirname( __FILE__ ).'/IDNA3.php' );
+	$idn = Net_IDNA3::getInstance();
 	$idn->setParams( 'utf8', true );
-	define( 'YOURLS_SITE_IDN', $idn->decode( YOURLS_SITE ) ); // eg http://héhé.net
+	define( 'YOURLS_SHORT_IDN', $idn->decode( YOURLS_SHORT_URL ) ); // eg http://héhé.net
 }
 
 // For the display
-yourls_add_filter( 'table_add_row', 'ozh_yidn_ace2idn' );
-yourls_add_filter( 'table_edit_row', 'ozh_yidn_ace2idn' );
-yourls_add_filter( 'html_title', 'ozh_yidn_ace2idn' );
-yourls_add_filter( 'html_link', 'ozh_yidn_ace2idn' );
-yourls_add_filter( 'bookmarklet_jsonp', 'ozh_yidn_ace2idn' );
-function ozh_yidn_ace2idn( $text ) {
-	if( strpos( $text, YOURLS_SITE ) !== false ) {
-		$text = str_replace( YOURLS_SITE, YOURLS_SITE_IDN, $text );
+yourls_add_filter( 'table_add_row', 'zcc_ssuidn_ace2idn' );
+yourls_add_filter( 'table_edit_row', 'zcc_ssuidn_ace2idn' );
+yourls_add_filter( 'html_title', 'zcc_ssuidn_ace2idn' );
+yourls_add_filter( 'html_link', 'zcc_ssuidn_ace2idn' );
+yourls_add_filter( 'bookmarklet_jsonp', 'zcc_ssuidn_ace2idn' );
+function zcc_ssuidn_ace2idn( $text ) {
+	if( strpos( $text, YOURLS_SHORT_URL ) !== false ) {
+		$text = str_replace( YOURLS_SHORT_URL, YOURLS_SHORT_IDN, $text );
 	}
 	return $text;
 }
 
 // For the Share Box
-yourls_add_filter( 'share_box_data', 'ozh_yidn_sharebox' );
-function ozh_yidn_sharebox( $in ) {
-	$in['share'] = ozh_yidn_ace2idn( $in['share'] );
-	$in['shorturl'] = ozh_yidn_ace2idn( $in['shorturl'] );
+yourls_add_filter( 'share_box_data', 'zcc_ssuidn_sharebox' );
+function zcc_ssuidn_sharebox( $in ) {
+	$in['share'] = zcc_ssuidn_ace2idn( $in['share'] );
+	$in['shorturl'] = zcc_ssuidn_ace2idn( $in['shorturl'] );
 	return $in;
-}
-
-// JS hack on the tools page to fix the Prefix n'Shorten explanation
-yourls_add_action( 'html_footer', 'ozh_yidn_footerjs' );
-function ozh_yidn_footerjs( $args ) {
-	$context = $args[0];
-	if( $context == 'tools' ) {
-		$string = str_replace( 'http://', '', YOURLS_SITE );
-		$idn = str_replace( 'http://', '', YOURLS_SITE_IDN );
-		echo <<<JS
-	<script>
-	(function($) {
-	    var omglol = $('div.sub_wrap').html().replace( '<span>$string/</span>', '<span>$idn/</span>');
-		$('div.sub_wrap').html( omglol );
-	})(jQuery);	
-	</script>
-JS;
-	}
 }
--- readme.txt
+++ readme.txt
@@ -1,30 +1,32 @@
-=== YOURLS IDN ===
-Plugin Name: YOURLS IDN
-Plugin URI:  http://yourls.org/
+=== Swap Short URL IDN ===
+Plugin Name: Swap Short URL IDN
+Plugin URI:  https://github.com/idkisdc/SwapShortURL-IDN
 Description: Compatibility with Internationalized Domain Names (IDN)
-Version:     1.0
-Author:      Ozh - http://ozh.org/
+Version:     1.00 b
+Author:      idkisdc - http://0by.Cc/
 
-THIS PLUGIN REQUIRES YOURLS 1.5.1-beta AT LEAST. 
+THIS PLUGIN REQUIRES YOURLS 1.5.1-beta AT LEAST.
+ONLY WORKS WITH "YOURLS SWAP SHORT URL" PLUGIN. IF MAIN URL IS ALSO AN IDN WILL STILL NEED "YOURLS IDN"
 
+This is a clone os Ozh's plugin to provide IDN support to YOURLS_SHORT_URL instead of YOURLS_SITE for the Swap Short Yourls Plugin both can be run in tandem if needed.
 == Installation ==
 
-* Create a directory named 'yourls-idn/' into your 'user/plugins/' directory
+* Create a directory named 'swapshorturl-idn/' into your 'user/plugins/' directory
 * Upload all files contained in this archive into that newly created directory
 * Head to your YOURLS admin area and the Plugins management page, then activate
-  the YOURLS IDN plugin.
+  the Yourls Swap Short URL IDN plugin.
     
 == Configuration ==
 
 Before you activate the plugin, you must ensure that your config.php is correct.
 
-The constant YOURLS_SITE must be set to your domain with ACE (Punnycode) notation.
+The constant YOURLS_SHORT_URL must be set to your domain with ACE (Punnycode) notation.
 It must not contain any UTF8 or funky character.
 
-For instance, if your YOURLS' URL is:
+For instance, if your Short URL is:
       http://héhé.com
 You must have:
-      define('YOURLS_SITE', 'http://xn--hh-bjab.com');
+      define('YOURLS_SHORT_URL', 'http://xn--hh-bjab.com');
 	  
 To convert from IDN to ACE (or Punnycode, the one that starts with xn--), you can
-use a tool like http://www.idnstuff.com/ or http://mct.verisign-grs.com/
+use a tool like http://www.idnstuff.com/ or http://mct.verisign-grs.com/or http://mct.verisign-grs.com/
